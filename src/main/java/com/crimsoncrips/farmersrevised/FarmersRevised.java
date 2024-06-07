0debe4f975478cbaa769f136ae716e75826424cc
Crimson Crips <danecarmona2@gmail.com>
2024-06-08 01:01:19 +0800

    Begin

---
+++ src/main/java/com/crimsoncrips/farmersrevised/FarmersRevised.java
@@ -0,0 +1,20 @@
+package com.crimsoncrips.farmersrevised;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(FarmersRevised.MODID)
+public class FarmersRevised {
+
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "farmersrevised";
+
+
+    public FarmersRevised() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
